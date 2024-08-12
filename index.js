7047907479c043b5e09d6ed86b3c8bb1f3d0dd2e
Welsley <welsleylui@gmail.com>
2024-08-13 04:04:47 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,7 @@
+import { getContext } from "../../extensions.js";
+
+const context = getContext();
+context.chat; // Chat log
+context.characters; // Character list
+context.groups; // Group list
+// And many more...
